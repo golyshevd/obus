46e62fbf9f507f4b252eba4466b51e77b340d82f
spacy <spacy@yandex-team.ru>
2015-07-06 17:21:55 +0300

    Move Obus.parse to a separate module

---
+++ parse.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var R_SEARCH = /^\s*([^\s])([\s\S]*)/;
+var R_DEGRADE = /^\s*[^\s.\[]/;
+var R_IDENT = /^\s*([_a-z$][\w$]*)([\s\S]*)$/i;
+var R_OPEN_ACCESS = /^\s*(?:(\d+)|(['"]))([\s\S]*)$/;
+var R_STRING1 = /^([^"]*)"([\s\S]*)$/;
+var R_STRING2 = /^([^']*)'([\s\S]*)$/;
+var R_CLOSE_ACCESS = /^\s*]([\s\S]*)$/;
+
+function parse(s) {
+    /*eslint  default-case: 0, complexity: 0*/
+    var orig = s;
+    var m;
+    var state = '?';
+    var parts = [];
+
+    if (R_DEGRADE.test(s)) {
+        state = '.';
+    }
+
+    while (state !== 'EOF') {
+
+        switch (state) {
+
+            case '?':
+                m = R_SEARCH.exec(s);
+
+                if (!m) {
+                    state = 'EOF';
+                    break;
+                }
+
+                s = m[2];
+                state = m[1];
+                break;
+
+            case '.':
+                m = R_IDENT.exec(s);
+
+                if (!m) {
+                    state = 'INVALID';
+                    break;
+                }
+
+                s = m[2];
+                parts[parts.length] = m[1];
+                state = '?';
+                break;
+
+            case '[':
+                m = R_OPEN_ACCESS.exec(s);
+
+                if (!m) {
+                    state = 'INVALID';
+                    break;
+                }
+
+                s = m[3];
+
+                if (m[1]) {
+                    parts[parts.length] = parseInt(m[1], 10);
+                    state = ']';
+                    break;
+                }
+
+                state = m[2];
+                break;
+
+            case '"':
+                m = R_STRING1.exec(s);
+
+                if (!m) {
+                    state = 'INVALID';
+                    break;
+                }
+
+                s = m[2];
+                parts[parts.length] = m[1];
+                state = ']';
+                break;
+
+            case '\'':
+                m = R_STRING2.exec(s);
+
+                if (!m) {
+                    state = 'INVALID';
+                    break;
+                }
+
+                s = m[2];
+                parts[parts.length] = m[1];
+                state = ']';
+                break;
+
+            case ']':
+                m = R_CLOSE_ACCESS.exec(s);
+
+                if (!m) {
+                    state = 'INVALID';
+                    break;
+                }
+
+                s = m[1];
+                state = '?';
+                break;
+
+            default:
+                throw new SyntaxError(orig + '\n' + new Array(orig.length - s.length + 17).join(' ') + '^');
+
+        }
+    }
+
+    return parts;
+}
+
+module.exports = parse;
